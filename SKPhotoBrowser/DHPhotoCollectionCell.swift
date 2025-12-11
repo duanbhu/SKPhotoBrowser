7356bdd61a36867288a391d2785356d085c328d3
dbh <310701836@qq.com>
2025-12-11 11:06:27 +0800

    1

--- SKPhotoBrowser/DHPhotoCollectionCell.swift
+++ SKPhotoBrowser/DHPhotoCollectionCell.swift
@@ -18,6 +18,16 @@ open class DHPhotoCollectionCell: UICollectionViewCell {
         super.init(frame: frame)
         
         contentView.addSubview(imageScrollView)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(handleSKPhotoLoadingDidEndNotification(_:)),
+                                               name: NSNotification.Name(rawValue: SKPHOTO_LOADING_DID_END_NOTIFICATION),
+                                               object: nil)
+    }
+    
+    open override func prepareForReuse() {
+        super.prepareForReuse()
+        imageScrollView.prepareForReuse()
     }
     
     open override func layoutSubviews() {
@@ -27,5 +37,22 @@ open class DHPhotoCollectionCell: UICollectionViewCell {
     
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Notification
+    @objc open func handleSKPhotoLoadingDidEndNotification(_ notification: Notification) {
+        guard let photo = notification.object as? SKPhotoProtocol else {
+            return
+        }
+        
+        DispatchQueue.main.async(execute: {
+            let page = self.imageScrollView
+            
+            if photo.underlyingImage != nil {
+                page.displayImage(complete: true)
+            } else {
+                page.displayImageFailure()
+            }
+        })
     }
 }
